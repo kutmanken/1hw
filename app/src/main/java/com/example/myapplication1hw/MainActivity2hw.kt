6f15d607bd3f0deb1fad0c8bd6d589c183509453
kutmanken <kutmanken@gmail.com>
2021-12-11 00:37:03 +0600

    ewvewvew

---
+++ app/src/main/java/com/example/myapplication1hw/MainActivity2hw.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication1hw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2hw : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activity2hw)
+    }
+}
